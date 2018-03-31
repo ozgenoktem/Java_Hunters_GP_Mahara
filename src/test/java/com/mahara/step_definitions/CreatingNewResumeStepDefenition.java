a6c5646a6e222e6cd217fbf508e13ad504c79d65
Tstepany8 <33642720+Tstepany8@users.noreply.github.com>
2018-03-31 13:37:08 -0400

    Step Definition

---
+++ src/test/java/com/mahara/step_definitions/CreatingNewResumeStepDefenition.java
@@ -0,0 +1,129 @@
+package com.mahara.step_definitions;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CreatingNewResumeStepDefenition {
+	@When("^User clicks on main menu button$")
+	public void user_clicks_on_main_menu_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	
+	@Then("^User clicks on content button$")
+	public void user_clicks_on_content_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User clicks on Resume button$")
+	public void user_clicks_on_Resume_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^following tab options should be visible for Resume:$")
+	public void following_tab_options_should_be_visible_for_Resume(DataTable arg1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc).
+	    // Field names for YourType must match the column names in 
+	    // your feature file (except for spaces and capitalization).
+	    throw new PendingException();
+	}
+
+	@Given("^User is on Resume page$")
+	public void user_is_on_Resume_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Introduction option is selected #default$")
+	public void introduction_option_is_selected_default() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user selects date of birth as \"([^\"]*)\"$")
+	public void user_selects_date_of_birth_as(String arg1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user selects Gender identity as \"([^\"]*)\" at the gender$")
+	public void user_selects_Gender_identity_as_at_the_gender(String arg1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user clicks Save Button$")
+	public void user_clicks_Save_Button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Education and employment option is selected$")
+	public void education_and_employment_option_is_selected() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Under Education history tab user clicks on Add button$")
+	public void under_Education_history_tab_user_clicks_on_Add_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Under Employment history tab user clicks on Add button$")
+	public void under_Employment_history_tab_user_clicks_on_Add_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user fills up the following fields in Education history tab:   :$")
+	public void user_fills_up_the_following_fields_in_Education_history_tab(DataTable arg1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc).
+	    // Field names for YourType must match the column names in 
+	    // your feature file (except for spaces and capitalization).
+	    throw new PendingException();
+	}
+
+	@Then("^user fills up the following fields in Employment history tab:$")
+	public void user_fills_up_the_following_fields_in_Employment_history_tab(DataTable arg1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc).
+	    // Field names for YourType must match the column names in 
+	    // your feature file (except for spaces and capitalization).
+	    throw new PendingException();
+	}
+
+	@Then("^Under Education history tab user clicks on Choose File button$")
+	public void under_Education_history_tab_user_clicks_on_Choose_File_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User uploads Resume$")
+	public void user_uploads_Resume() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User click Save button$")
+	public void user_click_Save_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
+// ts
+//step def
