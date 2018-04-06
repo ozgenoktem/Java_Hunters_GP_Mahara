461790f279b82ad0698775b05ace49294a7c950c
ozgenoktem <oktem.ozgen@gmail.com>
2018-04-06 10:27:02 -0400

    added smokeTest step definitions

---
+++ src/test/java/com/mahara/step_definitions/SmokeTestStepDefinitions.java
@@ -0,0 +1,33 @@
+package com.mahara.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+
+public class SmokeTestStepDefinitions {
+
+	
+
+@Then("^verify first name and user name input boxes$")
+public void verify_first_name_and_user_name_input_boxes() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^User click Profile Page button$")
+public void user_click_Profile_Page_button() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^click LogOut$")
+public void click_LogOut() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^verify you have been logged out successfully$")
+public void verify_you_have_been_logged_out_successfully() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+}
